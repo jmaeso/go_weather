ea1393ecb0b71f4f6855ba92628baf42107850b7
slipy12 <jmaeso@enti.cat>
2017-04-29 17:57:33 +0200

    Default endpoint for a hello at localhost:8080

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("go_weather demo!")
+	fmt.Println("`curl http://localhost:8080` --> Hello from server")
+	fmt.Println("Ctrl+c to stop the server")
+
+	http.HandleFunc("/", helloFunc) //Call to root path calls to helloFunc
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("You asked for nothing! Here you have a Hello World so. :D\n"))
 }
